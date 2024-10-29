eb3925b7c86905118736e72f3fea8811a9b32b65
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-10-29 15:32:11 +0100

    updt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,19 @@
 function HomeScreen() {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home-box">
+            <header className="landing">
+                <div className="text">
+                    <h1>A Modern Bank Card For A Modern World</h1>
+                    <p>
+                        The mordern bank card embraces the era of contactless payments. enabling swift and effortless transactions with just a tap or a wave. no more fumbling for cash or struggling with outdated payment methods
+                    </p>
+                    <section className="btn">
+                        <p>Explore More</p>
+                        <ion-icon name="return-up-forward-outline"></ion-icon>
+                    </section>
+                </div>
+                <div className="bg"></div>
+            </header>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import "./styles/reset.css"
 import "./styles/index.css"
+import "./styles/home.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
